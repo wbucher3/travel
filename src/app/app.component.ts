7a44ea711e92c8b8c4fce168fb89cdf3d9aeba95
William Bucher <wjbuhcer3@gmail.com>
2022-11-13 16:02:39 -0500

    added html form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'travel';
+
+  
+  
+  car: string = "";
+  miles: string = "";
+  miataMPG: number = 25;
+  jeepMPG: number = 18.5;
+
+  onSubmit() {
+    console.log(this.miles);
+    console.log(this.car)
+  }
+
+  calculateCost(miles: number, mpg: number) {
+
+  }
+
 }
