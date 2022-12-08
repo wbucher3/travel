c6e2687c12af9603294d37a479a87845f3ebd1a5
William Bucher <wjbuhcer3@gmail.com>
2022-12-07 20:19:25 -0500

    scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -7,21 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  list = {
+          1: {name:"Home", active: true},
+          2: {name:"Projects", active: false},
+          3: {name:"About Me", active: false},
+          4: {name:"Contact", active: false}
+        };
 
   
+  currentIndex = 0;
   
-  car: string = "";
-  miles: string = "";
-  miataMPG: number = 25;
-  jeepMPG: number = 18.5;
-
-  onSubmit() {
-    console.log(this.miles);
-    console.log(this.car)
+  @HostListener('window:scroll', ['$event'])
+  ping(input: any) {
+    // console.log(input.target.scrollingElement.scrollTop)
+    this.changeColor(input.target.scrollingElement.scrollTop)
+    return true;
   }
 
-  calculateCost(miles: number, mpg: number) {
-
+  changeColor(input: number) {
+    if (input > 0 && input < 150) {
+      this.list[1].active = true;
+      this.list[2].active = false;
+      this.list[3].active = false;
+      this.list[4].active = false;
+    } else if (input > 151 && input < 300) {
+      this.list[1].active = false;
+      this.list[2].active = true;
+      this.list[3].active = false;
+      this.list[4].active = false;
+    } else if (input > 301 && input < 450) {
+      this.list[1].active = false;
+      this.list[2].active = false;
+      this.list[3].active = true;
+      this.list[4].active = false;
+    } else if (input > 451) {
+      this.list[1].active = false;
+      this.list[2].active = false;
+      this.list[3].active = false;
+      this.list[4].active = true;
+    }
   }
 
 }
